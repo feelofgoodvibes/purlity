aa9a5243c741093bf5fee94ea20c57b0df053d2e
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-05-04 01:44:53 +0300

    Inital commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from database import db
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
