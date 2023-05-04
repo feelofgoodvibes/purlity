2613b518d85c9e74eed368f5859fccd4f4b300f8
Maskym Kochut <feelofgoodvibes@gmail.com>
2023-05-05 01:51:23 +0300

    Add testing app configuration

--- src/app.py
+++ src/app.py
@@ -2,10 +2,13 @@ from flask import Flask
 from src.database import db
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if config is None:
+        app.config.from_object("src.app_configuration.Config")
+    else:
+        app.config.from_object(config)
 
     db.init_app(app)
 
