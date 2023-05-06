d583abfeca009ea380cfab4e34f08203111bb882
Maskym Kochut <feelofgoodvibes@gmail.com>
2023-05-06 14:27:02 +0300

    Add visit service tests

---
+++ tests/test_service_visit.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import pytest
+
+from tests.fixtures import dummy_db, visit_service
+from src.service.visit import VisitService
+
+
+def test_visit_get(visit_service: VisitService):
+    visit = visit_service.get_visit(2)
+    assert visit.date == datetime(2023, 5, 1, 22, 12, 00)
+
+    with pytest.raises(ValueError):
+        visit_service.get_visit(50)
+
+def test_visit_get_all(visit_service: VisitService):
+    assert len(visit_service.get_all_visits()) == 16
+
+def test_visit_create(visit_service: VisitService):
+    visit = visit_service.create_visit(short_url="gitHB")
+
+    assert len(visit_service.get_all_visits()) == 17
