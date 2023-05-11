023a184d43d84f265a4d142d6215a7c60dc77f85
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-05-11 06:42:27 +0300

    Add SECRET_KEY to configuration

--- src/app_configuration.py
+++ src/app_configuration.py
@@ -1,9 +1,11 @@
 class Config:
+    SECRET_KEY = '0d99f6a17b6548608f5b408725edeec0jjiewu321oi54jos'
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConfig:
     TESTING = True
+    SECRET_KEY = '0d99f6a17b6548608f5b408725edeec0jjiewu321oi54jos'
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
