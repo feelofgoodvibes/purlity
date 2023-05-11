0af08deaa6283d643395154987ebdd6ea60fa37e
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-05-11 17:56:57 +0300

    Add user_identity_loader and user_lookup_loader to JWT manager

--- src/jwt.py
+++ src/jwt.py
@@ -1,3 +1,17 @@
 from flask_jwt_extended import JWTManager
+from src.database import db
+from src.service.user import UserService
 
 jwt_manager = JWTManager()
+user_service = UserService(db)
+
+
+@jwt_manager.user_identity_loader
+def user_identity_loader(user):
+    return user.id
+
+
+@jwt_manager.user_lookup_loader
+def user_lookup_loader(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return user_service.get_user(identity)
