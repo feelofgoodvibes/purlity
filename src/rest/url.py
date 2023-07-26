5778faa4991a4aa43506869579f5adf30ee52e62
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-07-27 01:18:41 +0300

    Add GET function to URL Resource

--- src/rest/url.py
+++ src/rest/url.py
@@ -1,9 +1,9 @@
 from flask import jsonify
-from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, current_user
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_current_user
 from src.database import db
 from src.service.url import URLService
-from src.schemas.url import URL as URLSchema
+from src.schemas import url as url_schemas
 
 
 class URL(Resource):
@@ -11,8 +11,16 @@ class URL(Resource):
 
     @jwt_required(optional=True)
     def get(self, short_url: str):
+        current_user = get_current_user()
+
         try:
             url = self.url_service.get_url(short_url)
-            return jsonify(URLSchema.from_orm(url).dict())
         except ValueError as e:
             return {"error": str(e)}
+        
+        if current_user is None:
+            return jsonify(url_schemas.URL.from_orm(url).dict())
+        else:
+            return jsonify(url_schemas.URLAuthenticated.from_orm(url).dict())
+
+
