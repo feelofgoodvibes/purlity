3ab7a882c83113f6d2bebe4e57aee04aa88841d9
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-07-27 00:42:46 +0300

    Connect URLCollection and URL Resources to REST API

--- src/rest/__init__.py
+++ src/rest/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
 from src.rest.authentication import Registration, Login
+from src.rest.url import URL
+from src.rest.url_collection import URL_collection
 
 
 api_blueprint = Blueprint("api", __name__)
@@ -8,5 +10,7 @@ api_blueprint = Blueprint("api", __name__)
 api = Api()
 api.add_resource(Registration, "/register")
 api.add_resource(Login, "/login")
+api.add_resource(URL_collection, '/url')
+api.add_resource(URL, '/url/<string:short_url>')
 
 api.init_app(api_blueprint)
