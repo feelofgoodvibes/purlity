960bdf1de876900878ebe44b7777c6777b704236
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-07-27 01:19:12 +0300

    Add tests to URL Resource GET function

--- tests/rest/test_rest_url.py
+++ tests/rest/test_rest_url.py
@@ -1,7 +1,7 @@
 from tests.fixtures import dummy_db, test_app, test_client, test_jwt
 
 
-def test_url_get(test_client, test_jwt):
+def test_url_collection_get(test_client, test_jwt):
     get_no_auth = test_client.get("/api/url")
     get_auth = test_client.get("/api/url", headers={"Authorization": test_jwt})
     get_filters = test_client.get("/api/url", headers={"Authorization": test_jwt}, query_string={"limit":1})
@@ -13,8 +13,20 @@ def test_url_get(test_client, test_jwt):
     assert len(get_filters_2.json) == 1 and get_filters_2.json[0]["short_url"] == "YouTb"
 
 def test_url_creation(test_client, test_jwt):
-    create_unauthenticated = test_client.post("/api/url", data={"url": "https://hello.com"})
-    create_authenticated = test_client.post("/api/url", data={"url": "https://hello2.com"}, headers={"Authorization": test_jwt})
+    create_unauth = test_client.post("/api/url", data={"url": "https://hello.com"})
+    assert create_unauth.json["url"] == "https://hello.com"
 
-    assert create_unauthenticated.json["user_id"] == None
-    assert create_authenticated.json["user_id"] == 1
+    check_before = test_client.get("/api/url", headers={"Authorization": test_jwt})
+    create_auth = test_client.post("/api/url", data={"url": "https://hello2.com"}, headers={"Authorization": test_jwt})
+    check_after = test_client.get("/api/url", headers={"Authorization": test_jwt})
+    assert create_auth.json["url"] == "https://hello2.com"
+    assert len(check_after.json) - len(check_before.json) == 1
+
+def test_url_get(test_client, test_jwt):
+    get_noauth = test_client.get("/api/url/pyThN")
+    get_auth = test_client.get("/api/url/pyThN", headers={"Authorization": test_jwt})
+    get_invalid = test_client.get("/api/url/xxXXxx")
+
+    assert "visits" not in get_noauth.json
+    assert "visits" in get_auth.json
+    assert "error" in get_invalid.json and get_invalid.json["error"].endswith("not found!")
