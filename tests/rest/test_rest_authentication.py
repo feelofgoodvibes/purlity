8cd2377600f6f130bda9523709965a70e2cb62b7
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-07-27 01:38:22 +0300

    Fix tests after changing message error key to "msg"

--- tests/rest/test_rest_authentication.py
+++ tests/rest/test_rest_authentication.py
@@ -5,21 +5,24 @@ def test_authentication_process(test_client):
     user_data =  {"username": "test_username", "password": "test_password"}
 
     register_response = test_client.post("/api/register", data=user_data)
-    login_response = test_client.post("/api/login", data=user_data)
-    wrong_passwrod_response = test_client.post("/api/login", data={"username": user_data["username"], "password": "12345"})
+    assert register_response.json["msg"] == "ok"
 
-    assert register_response.json["message"] == "ok"
+    login_response = test_client.post("/api/login", data=user_data)
     assert "access_token" in login_response.json
-    assert wrong_passwrod_response.json["message"] == "Wrong password"
+
+    wrong_passwrod_response = test_client.post("/api/login", data={"username": user_data["username"], "password": "12345"})
+    assert wrong_passwrod_response.json["msg"] == "Wrong password"
 
 
 def test_authentication_errors(test_client):
     unique_username_response = test_client.post("/api/register", data={"username": "Max", "password": "123"})
-    missing_username_response = test_client.post("/api/register", data={"password": "123"})
-    missing_password_response = test_client.post("/api/register", data={"username": "123"})
-    nonexisting_response = test_client.post("/api/login", data={"username": "123", "password": "123"})
+    assert unique_username_response.json["msg"] == "User with username Max already exists!"
 
-    assert unique_username_response.json["message"] == "User with username Max already exists!"
+    missing_username_response = test_client.post("/api/register", data={"password": "123"})
     assert missing_username_response.json["message"]["username"] == "Username is required"
+
+    missing_password_response = test_client.post("/api/register", data={"username": "123"})
     assert missing_password_response.json["message"]["password"] == "Password is required"
-    assert nonexisting_response.json["message"] == "User does not exist"
+
+    nonexisting_response = test_client.post("/api/login", data={"username": "123", "password": "123"})
+    assert nonexisting_response.json["msg"] == "User does not exist"
