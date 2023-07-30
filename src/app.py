72a66c07cd1a344876029611daec369cd9ba482f
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-07-30 03:51:45 +0300

    Registred web application blueprint in main application

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
+from src.app_configuration import OrJSONProvider
 from src.database import db, migrate
-from src.rest import api_blueprint
+
 from src.models import *
 from src.jwt import jwt_manager
-from src.app_configuration import OrJSONProvider
+from src.rest import api_blueprint
+from src.views import webapp_blueprint
 
 
 def create_app(config=None):
@@ -19,6 +22,7 @@ def create_app(config=None):
     migrate.init_app(app)
     jwt_manager.init_app(app)
 
+    app.register_blueprint(webapp_blueprint)
     app.register_blueprint(api_blueprint, url_prefix="/api")
 
     return app
