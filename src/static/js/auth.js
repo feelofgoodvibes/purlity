7b8515f2d6edb09970cae44d58411721e15e632d
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-08-08 06:52:49 +0300

    Add login and registration process to javascript

--- src/static/js/auth.js
+++ src/static/js/auth.js
@@ -1,8 +1,45 @@
 window.addEventListener("load", () => {
     if ($("#authform[type='login']")[0]){
-        
-    }
-    else if ($("#authform[type='registration']")[0]){
+        $("#authform-submit").on("click", () => {
+            let username = $("input[name='username']")[0].value;
+            let password = $("input[name='password']")[0].value;
 
+            $.ajax({
+                url: urls.login,
+                method: "POST",
+                data: {
+                    "username": username,
+                    "password": password},
+                success: (resp) => {
+                    location.href = '/';
+                },
+                error: (resp) => {
+                    $.notify(resp.responseJSON.msg, "error");
+                }
+            });
+        });
+    }
+
+    else if ($("#authform[type='registration']")[0]){
+        $("#authform-submit").on("click", () => {
+            let username = $("input[name='username']")[0].value;
+            let password = $("input[name='password']")[0].value;
+            
+            $.ajax({
+                url: urls.register,
+                method: "POST",
+                data: {
+                    "username": username,
+                    "password": password},
+                success: (resp) => {
+                    location.href = '/';
+                },
+                error: (resp) => {
+                    $.notify(resp.responseJSON.msg, "error");
+
+                }
+
+            });
+        });
     }
 });
