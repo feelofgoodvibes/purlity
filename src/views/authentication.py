0d0e08cc886374f91cdd940d3ea1dba047291309
Maksym Kochut <feelofgoodvibes@gmail.com>
2023-08-10 23:22:52 +0300

    Logging out user when it visits login or register pages

--- src/views/authentication.py
+++ src/views/authentication.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, make_response
+from flask_jwt_extended import get_current_user, unset_jwt_cookies, jwt_required
 
+@jwt_required(optional=True)
 def login():
-    return render_template("login.html")
+    current_user = get_current_user()
+    response = make_response(render_template("login.html"), 200)
 
+    if current_user is not None:
+        unset_jwt_cookies(response)
+
+    return response
+
+@jwt_required(optional=True)
 def register():
-    return render_template("register.html")
+    current_user = get_current_user()
+    response = make_response(render_template("register.html"), 200)
+
+    if current_user is not None:
+        unset_jwt_cookies(response)
+
+    return response
